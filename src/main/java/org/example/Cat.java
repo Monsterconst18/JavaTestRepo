a85f1c7d5dd4c335d732063bf77c537c1cc8c4cf
6241253 <constantin.kalantzis@gmail.com>
2024-04-22 14:11:22 -0400

    Initialize the project

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Cat {
+    private int age;
+    private String name;
+}
